b98afd76a09909e57d3501b054af2b09a46d1c8b
mcasrom <mcasrom@gmail.com>
2026-01-29 12:43:41 +0100

    Fix: favicon, preview y alertas

---
+++ watch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script para previsualización instantánea en el Vivobook
+echo "📡 Iniciando radar de cambios en El Mapa y El Código..."
+hugo server -D --disableFastRender --bind 0.0.0.0
